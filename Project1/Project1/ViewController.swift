e83d97bc5a3928431df318c36c5cccebf0be1e28
alhe9379 <59906050+alhe9379@users.noreply.github.com>
2021-10-10 12:33:53 -0600

    Add files via upload

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -11,13 +11,17 @@ import PencilKit
 
 //https://www.raywenderlich.com/12198216-drawing-with-pencilkit-getting-started
 //https://medium.com/flawless-app-stories/getting-started-with-pencilkit-on-ios-13-a4bda3323fd8
+//https://www.hackingwithswift.com/example-code/media/how-to-choose-a-photo-from-the-camera-roll-using-uiimagepickercontroller
+//https://theswiftdev.com/picking-images-with-uiimagepickercontroller-in-swift-5/
+//https://github.com/theswiftdev/tutorials/tree/master/iOS/Pickers
 class ViewController: UIViewController, PKToolPickerObserver {
     
     @IBOutlet weak var imgView: UIImageView!
     var canvasView: PKCanvasView!
     var imgForMarkup: UIImage?
     var toolPicker = PKToolPicker.init()
-    
+    var imagePicker = UIImagePickerController.init()
+   
     var origin: CGPoint?
     var screenSize: CGRect?
     var screenWidth: CGFloat?
@@ -39,14 +43,11 @@ class ViewController: UIViewController, PKToolPickerObserver {
         self.canvasView.frame = CGRect.init( x: CGFloat.init(), y: CGFloat.init(), width: screenWidth!, height: screenHeight!)
         self.view.addSubview(self.canvasView)
         
-
         //https://medium.com/flawless-app-stories/getting-started-with-pencilkit-on-ios-13-a4bda3323fd8
         toolPicker.setVisible(true, forFirstResponder: canvasView)
         toolPicker.addObserver(canvasView)
         toolPicker.addObserver(self)
         canvasView.becomeFirstResponder()
-        
-        
         self.canvasView.drawingPolicy = .anyInput
         
     }
