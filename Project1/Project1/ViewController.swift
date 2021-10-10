d332fecdc227e98a7641f5b926c20ebd6da3baeb
alhe9379 <59906050+alhe9379@users.noreply.github.com>
2021-10-10 14:26:14 -0600

    Add files via upload

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -14,33 +14,31 @@ import PencilKit
 //https://www.hackingwithswift.com/example-code/media/how-to-choose-a-photo-from-the-camera-roll-using-uiimagepickercontroller
 //https://theswiftdev.com/picking-images-with-uiimagepickercontroller-in-swift-5/
 //https://github.com/theswiftdev/tutorials/tree/master/iOS/Pickers
-class ViewController: UIViewController, PKToolPickerObserver {
+class ViewController: UIViewController, PKToolPickerObserver, ImagePickerDelegate {
+    func didSelect(image: UIImage?) {
+        guard let image = image else {
+            return
+        }
+        self.imgView.image = image
+    }
+    
     
     @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var ImagePickerButton: UIButton!
+    var imagePicker: ImagePicker!
+    
     var canvasView: PKCanvasView!
     var imgForMarkup: UIImage?
-    var toolPicker = PKToolPicker.init()
-    var imagePicker = UIImagePickerController.init()
-   
-    var origin: CGPoint?
-    var screenSize: CGRect?
-    var screenWidth: CGFloat?
-    var screenHeight: CGFloat?
+    var toolPicker = PKToolPicker.init()   
     
     override func viewDidLoad() {
-        //https://stackoverflow.com/questions/24110762/swift-determine-ios-screen-size
         super.viewDidLoad()
-        origin = CGPoint.init(x:0, y:0)
-        screenSize = UIScreen.main.bounds
-        screenWidth = screenSize!.width
-        screenHeight = screenSize!.height
-        // Do any additional setup after loading the view.
     }
     
     override func viewDidAppear(_ animated: Bool) {
         self.canvasView = PKCanvasView.init(frame: self.imgView.frame)
         self.canvasView.isOpaque = false
-        self.canvasView.frame = CGRect.init( x: CGFloat.init(), y: CGFloat.init(), width: screenWidth!, height: screenHeight!)
+        self.canvasView.frame = CGRect.init( x: imgView.frame.origin.x, y: imgView.frame.origin.y, width: imgView.frame.width, height: imgView.frame.height)
         self.view.addSubview(self.canvasView)
         
         //https://medium.com/flawless-app-stories/getting-started-with-pencilkit-on-ios-13-a4bda3323fd8
@@ -51,6 +49,14 @@ class ViewController: UIViewController, PKToolPickerObserver {
         self.canvasView.drawingPolicy = .anyInput
         
     }
+      
+    //This is entirely ripped from that dudes github... :(
+    //Time to figure out how tf it works and make it work myself
+    @IBAction func imagePickerButton(_ sender: UIButton) {
+        self.imagePicker = ImagePicker(presentationController: self, delegate: self)
+        self.imagePicker.present(from: sender)
+    }
+    
     
 }
 
